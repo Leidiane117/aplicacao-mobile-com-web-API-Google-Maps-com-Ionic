0e6e6bdb401499656b88f44a8e8b70b5632bf2b5
Leidiane <Leidiane117@users.noreply.github.com>
2020-12-05 11:09:27 -0300

    obter endereço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {NativeGeocoder} from '@ionic-native/native-geocoder/ngx';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation,
+   NativeGeocoder
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult} from '@ionic-native/native-geocoder/ngx';
 
+
+declare var google: any; // varíavel global, da api do google
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(
+    'map', { static: false})mapElement: ElementRef; // decorator para mandar mostrar o mapa na nossa div
+  map: any; // que irá receber nosso map
+  latitude: number;
+  longitude: number;
+  endereco: any;
+  constructor(
+    private  geolocation: Geolocation, private nativeGeocoder: NativeGeocoder
+    )
+  {}
 
-  constructor() {}
+  lerMapa(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      //let latitude = 23.5227;
+      //let longitude = -46.8368; // variáveis locais;
+     this.latitude = resp.coords.latitude; // coordenadas da minha localozação 
+     this.longitude = resp.coords.longitude;
 
-}
+     const latinlng = new google.maps.LatLng(this.latitude, this.longitude);
+
+     const mapOptions = // configurações do mapa 
+     {
+       center : latinlng,
+       zoom : 15,
+      // mapTypeId : google.maps.mapTypeId.ROADMAP // na hora de compilar estava dando undefined, por isso retirei o ROADMap
+     }
+
+   this.map = new google.maps.Map (this.mapElement.nativeElement, mapOptions);
+    this.map.addListener('dragend', () => { // atributo map que irá receber o método da api google maps
+      this.latitude = this.map.center.lat(); // listener que atualiza de acordo com a posição do mapa
+      this.longitude = this .map.center.lng();
+     });
+    new google.maps.Marker ( { // adicionando um marcador da localização pesquisada
+     position: new google.maps.LatLng(this.latitude, this.longitude),
+     title: 'Pin Point',
+     map: this.map,
+     icon: 'assets/img/point.png' // imagem do meu marcador
+    });
+    
+  this.obterEndereco(this.latitude, this.longitude);
+   }); // chamando meu métpdo que vai obter o endereço 
+    
+  }
+  obterEndereco(latitude: number, longitude: number) {
+    console.log("Coordenadas: " + latitude + " " + longitude);
+    const options: NativeGeocoderOptions = {
+       useLocale: true,
+       maxResults: 5
+      };
+
+     this.nativeGeocoder.reverseGeocode(latitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.endereco = '';
+        const responseAddress = [];
+        for (const [key, value] of Object.entries(result[0])) {
+          if (value.length > 0) { // se valor maior que 0/ valor da pesquisa
+            responseAddress.push(value); // alimentar o array 
+          }
+        }
+        responseAddress.reverse();
+        this.endereco = responseAddress[2] + ', ' +
+          responseAddress[1] + ' ' +
+          responseAddress[3] + ' ' +
+          responseAddress[4] + ' ' +
+          responseAddress[5] + ' ' +
+          responseAddress[6] + ' ' +
+          responseAddress[7] + ' ' +
+          responseAddress[8];
+      })
+      .catch((error: any) => {
+        this.endereco = 'Endereço não disponível!'; // caso de erro
+      });
+ 
+    }
+
+  }
+
